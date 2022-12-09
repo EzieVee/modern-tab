c75eb624be7adac8f03d5e9a7aa348ac07217e3c
Erwin <EzieV@tuta.io>
2022-12-08 22:41:33 -0800

    Portfolio before animations

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -9,10 +9,10 @@ const Portfolio:React.FC<PortfolioProps> = () => {
   return (
     <div className="flex flex-col justify-center bg-black/70">
 
-<div className="h-[50vh] flex flex-col items-center justify-center text-center text-white">
-  <h4>1</h4>
-  <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, repellendus?</h1>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt obcaecati illum, ab omnis asperiores provident id maiores neque suscipit deleniti eum? Nihil aliquam facere atque soluta, nemo perspiciatis exercitationem repellendus adipisci commodi animi, quis aspernatur cumque necessitatibus dolore nesciunt harum.</p>
+<div className="h-[50vh] flex flex-col items-center justify-center text-center text-white px-[30rem] space-y-8">
+  <h4 className="mulishFont mt-10 text-orange-300 uppercase font-thin text-sm tracking-[1]">Portfolio</h4>
+  <h1 className="font-extrabold text-5xl text-white">Take A Look At Some of The Work We've Done.</h1>
+  <p className="text-gray-400 font-thin">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt obcaecati illum, ab omnis asperiores provident id maiores neque suscipit deleniti eum? Nihil aliquam facere atque soluta, nemo perspiciatis exercitationem repellendus adipisci commodi animi, quis aspernatur cumque necessitatibus dolore nesciunt harum.</p>
 </div>
 
 <div className="h-[100vh] bg-green-500"> 
